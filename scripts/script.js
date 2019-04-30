1dc4721671a6b7bcaf7258b729bc3c98e8727e1d
Damian Sikora <dsikora08@gmail.com>
2019-04-30 19:01:18 +0200

    Wersja ostateczna

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,9 @@ function main(){
     
 	$taskList = document.getElementById("task_list");	
 	$newTask = document.getElementById("edit_task");
-	$confirmButton = document.getElementById("confirm_button");
-	
+	$confirmButton = document.getElementById("confirm_button");	
 	$taskList.addEventListener('click', listClickManager);
-    $confirmButton.addEventListener('click',addTaskButtonClickHandler);
-    
+    $confirmButton.addEventListener('click',addTaskButtonClickHandler);    
     getTodos();
 }
 
@@ -33,9 +31,7 @@ function getTodos(){
 function addTaskButtonClickHandler(){
 
 	var newTask=$newTask.value;
-
 	if(newTask) {
-
         axios.post('http://195.181.210.249:3000/todo/', {
             title: newTask,
             author: 'Damian-Mechanik',
@@ -53,7 +49,6 @@ function addUpdateListTask(list,task_tekst,id,stan_zadania){
 
     var $newItemsList= document.createElement('li');
     $newItemsList.dataset.id=id;
-    debugger;
     var checkbox_element = document.createElement('INPUT');
     checkbox_element.setAttribute("type", "checkbox");
     if(stan_zadania ==='Wykonane')
@@ -61,51 +56,38 @@ function addUpdateListTask(list,task_tekst,id,stan_zadania){
         checkbox_element.checked=true;
         $newItemsList.classList.toggle('one_list_task_accept');
     }
-
 	var textElement = document.createElement('span');
-
 	 textElement.textContent = task_tekst;
-
 	var editButton = document.createElement('button');
-
 	editButton.textContent = 'Edycja';
     editButton.classList.add('button_to_edit_element_list');
-
     var cancelElement = document.createElement('button');
     cancelElement.textContent = 'Usuń';
     cancelElement.classList.add('cancel');
-
-
     var acceptButton = document.createElement('button');
-
     acceptButton.textContent = 'Zatwierdz';
     acceptButton.classList.add('accept');
 	acceptButton.style.display = 'none';
-
 	var changeInput = document.createElement('input');
     changeInput.style.display = 'none';
-
     $newItemsList.appendChild(checkbox_element);
     $newItemsList.appendChild(textElement);
     $newItemsList.appendChild(changeInput);
-
     $newItemsList.appendChild(acceptButton);
     $newItemsList.appendChild(editButton);
     $newItemsList.appendChild(cancelElement);
 
-	debugger;
 	list.appendChild($newItemsList);
  }
 
 function editClickHandler(event,$textElement,$changeInput,$acceptButton) {
-    debugger;
+   
     var oldName = $textElement.textContent;
     $textElement.style.display = 'none';
     event.target.style.display = 'none';
     $changeInput.value = oldName;
     $changeInput.style.display = 'inline-block';
-    $acceptButton.style.display = 'inline-block';
-    debugger;
+    $acceptButton.style.display = 'inline-block';   
 }
 
 function acceptChangeHandler(event,$textElement,$changeInput,$acceptButton) {
@@ -116,13 +98,9 @@ function acceptChangeHandler(event,$textElement,$changeInput,$acceptButton) {
     $textElement.textContent = $changeInput.value;
     $textElement.style.display = '';
     $editButton.style.display = 'inline-block';
-
-    debugger;
     $idElementToDo=event.target.parentElement.dataset.id;
     axios.put('http://195.181.210.249:3000/todo/' + $idElementToDo,{
-
         title: $textElement.textContent,
-
     })
     .then(function (){
         $taskList.innerHTML = '';
@@ -131,23 +109,18 @@ function acceptChangeHandler(event,$textElement,$changeInput,$acceptButton) {
 }
 
 function canelTaskHandler(event) {
-     $idElementToDo=event.target.parentElement.dataset.id;
-    debugger;
+    $idElementToDo=event.target.parentElement.dataset.id;
     axios.delete('http://195.181.210.249:3000/todo/' + $idElementToDo, {})
     .then(function () {
         $taskList.innerHTML = '';
         getTodos();
-      });   
-          
+    });             
 }
-
 
 function updateStatusOfTask(list, status, idElement){
 
     axios.put('http://195.181.210.249:3000/todo/' + idElement,{
-
         description: status
-        
     })
     .then(function (){
     list.innerHTML = '';
@@ -160,26 +133,20 @@ function listClickManager(event) {
     var $textElement = event.target.parentElement.getElementsByTagName('span')[0];
     var $changeInput = event.target.parentElement.getElementsByTagName('input')[1];
     var $acceptButton = event.target.parentElement.getElementsByClassName('accept')[0];
-
-	if(event.target.classList.contains('button_to_edit_element_list')) {
-        editClickHandler(event,$textElement,$changeInput,$acceptButton);
-    } else if(event.target.classList.contains('accept')) {
-
-    	acceptChangeHandler(event,$textElement,$changeInput,$acceptButton);
-
-    } else if(event.target.classList.contains('cancel')){
     
+    if(event.target.classList.contains('button_to_edit_element_list')) {
+        editClickHandler(event,$textElement,$changeInput,$acceptButton);       
+    } else if(event.target.classList.contains('accept')) {
+    	acceptChangeHandler(event,$textElement,$changeInput,$acceptButton);
+    } else if(event.target.classList.contains('cancel')){
     	canelTaskHandler(event);
-
     }else{
 
-	    if(event.target.getAttribute('type') === 'checkbox') {
-           
-              event.target.parentElement.classList.toggle('one_list_task_accept'); 
-              $idElementToDo=event.target.parentElement.dataset.id;
-
-              if(event.target.checked)
-              {
+	    if(event.target.getAttribute('type') === 'checkbox') {       
+            event.target.parentElement.classList.toggle('one_list_task_accept'); 
+            $idElementToDo=event.target.parentElement.dataset.id;
+            if(event.target.checked)
+            {
                 updateStatusOfTask($taskList,"Wykonane",$idElementToDo); 
             }else
             {
@@ -189,5 +156,3 @@ function listClickManager(event) {
 	}
 }
 document.addEventListener('DOMContentLoaded', main);
-
-
